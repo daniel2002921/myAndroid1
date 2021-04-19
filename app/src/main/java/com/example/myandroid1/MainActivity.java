93fc45d3e2188ecf7cdb64e915aaba41d8f46e8d
daniel <daniel2002921@gmail.com>
2021-04-19 18:06:14 +0800

    V1 新增Log

--- app/src/main/java/com/example/myandroid1/MainActivity.java
+++ app/src/main/java/com/example/myandroid1/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myandroid1;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.v("Daniel","一切OK");
+
     }
+
 }
